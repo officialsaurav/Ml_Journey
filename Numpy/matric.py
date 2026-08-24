b9422a630c623a6ed3a12b0e062f9682f880c05f
officialsaurav <officialsauravg@gmail.com>
2026-08-24 21:13:38 +0545

    matric in numpy.

---
+++ Numpy/matric.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create a 2x3 matrix (2 rows, 3 columns)
+matrix = np.array([[1, 2, 3], 
+                   [4, 5, 6]])
+
+print(matrix)
+# Output:
+# [[1 2 3]
+#  [4 5 6]]
+# Assuming a 2x3 matrix
+print(matrix.ndim)   # Dimensions: Returns 2
+print(matrix.shape)  # Shape: Returns (2, 3) (rows, columns)
+print(matrix.size)   # Total elements: Returns 6
