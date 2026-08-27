628d6c4e903645598eb0338b62f0db0d0e95c3de
officialsaurav <officialsauravg@gmail.com>
2026-08-27 23:06:41 +0545

    minor change in code, panda

---
+++ pandas/practice1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "name": ["Alice", "Bob", "Charlie", "David", "Eva"],
+    "age": [20, 25, 22, 30, 24],
+    "score": [85, 72, 91, 68, 88]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
---
+++ pandas/student_analysis.py
@@ -0,0 +1,6 @@
+
+import sys
+import pandas as pd
+
+print(sys.executable)
+print(pd.__version__)
