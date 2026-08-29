3539ca5ea519e79ffb1ebaedf5060720fb1c113b
officialsaurav <officialsauravg@gmail.com>
2026-08-29 23:13:08 +0545

    matrics

--- Numpy/matric.py
+++ Numpy/matric.py
@@ -9,6 +9,9 @@ print(matrix)
 # [[1 2 3]
 #  [4 5 6]]
 # Assuming a 2x3 matrix
+
 print(matrix.ndim)   # Dimensions: Returns 2
+
 print(matrix.shape)  # Shape: Returns (2, 3) (rows, columns)
+
 print(matrix.size)   # Total elements: Returns 6
